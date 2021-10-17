4d2f454cbff0ba71fefe42b407bb9a3834fe0568
Bora HIDIR <csehofficial@protonmail.com>
2021-10-17 19:17:24 +0300

    Add files via upload

---
+++ firstprogram.ino
@@ -0,0 +1,23 @@
+#define kbd_tr_tr
+#include "DigiKeyboard.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("notepad.exe");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("Merhaba Udemy");
+  DigiKeyboard.delay(10000);
+
+}
